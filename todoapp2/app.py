4242a39e8161c3432947e99dd0221da5dc67f4fe
Cecilia7337 <sunrui7337@gmail.com>
2022-09-21 09:20:03 -0700

    Added AJAX to send data to flask

--- todoapp2/app.py
+++ todoapp2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,6 +14,30 @@ class Todo(db.Model):
         return f'<Todo {self.id} {self.description}>'
     
 db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    #print(request)
+    #print(request.form)
+    print(request.get_json())
+    description = request.get_json()['description']
+    print(1)
+    todo = Todo(description=description)
+    print(2)
+
+    db.session.add(todo)
+    print(3)
+
+    db.session.commit()
+    print(4)
+    print(todo.description)
+    print(jsonify({
+        'description': todo.description
+    }))
+
+    return jsonify({
+        'description': todo.description
+    })
 
 @app.route('/')
 def index():
